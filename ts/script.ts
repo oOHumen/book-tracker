6ee069031f7b93f8f603022b26ef05c8d84b600e
Oleksandr Humeniuk <oleksandr.humeniuk@wpp.com>
2025-07-06 23:16:03 +0300

    prettier added

--- ts/script.ts
+++ ts/script.ts
@@ -1,4 +1,4 @@
-import { Book } from "./types/book";
+import { Book } from './types/book';
 
 const form = document.querySelector('form') as HTMLFormElement;
 
@@ -6,20 +6,23 @@ form.addEventListener('submit', (event) => {
     event.preventDefault();
     const bookName = (document.getElementById('bookname') as HTMLInputElement).value.trim();
     const bookauthor = (document.getElementById('bookauthor') as HTMLInputElement).value.trim();
-    const bookdescription = (document.getElementById('bookdescription') as HTMLTextAreaElement).value.trim();
+    const bookdescription = (
+        document.getElementById('bookdescription') as HTMLTextAreaElement
+    ).value.trim();
     const bookStatus = (document.getElementById('bookstatus') as HTMLSelectElement).value;
 
-    if (!bookName || !bookauthor)
-        { alert('Будь ласка, заповніть Назву книги та Автора') }
-    else {
+    if (!bookName || !bookauthor) {
+        alert('Будь ласка, заповніть Назву книги та Автора');
+    } else {
         const enteredBook: Book = { bookName, bookauthor, bookdescription, bookStatus };
         const savedBooks: Book[] = JSON.parse(localStorage.getItem('savedBooks')!) || [];
-        savedBooks.push(enteredBook)
-        localStorage.setItem('savedBooks', JSON.stringify(savedBooks))
+        savedBooks.push(enteredBook);
+        localStorage.setItem('savedBooks', JSON.stringify(savedBooks));
     }
 
     form.reset();
 
-    if (bookName && bookauthor) { alert('Книгу додано!') };
+    if (bookName && bookauthor) {
+        alert('Книгу додано!');
+    }
 });
-
